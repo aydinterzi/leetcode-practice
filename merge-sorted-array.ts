45579a4b1f2dab3562ea7718162eee358fae5120
Aydin Terzi <aydinterzi7@gmail.com>
2024-03-13 23:41:00 +0300

    init: merge sorted array

---
+++ merge-sorted-array.ts
@@ -0,0 +1,17 @@
+function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+  let p1: number = m - 1;
+  let p2: number = n - 1;
+  let k: number = nums1.length - 1;
+
+  while (p2 >= 0) {
+    if (nums1[p1] > nums2[p2]) {
+      nums1[k] = nums1[p1];
+      p1--;
+    } else {
+      nums1[k] = nums2[p2];
+      p2--;
+    }
+    k--;
+  }
+  console.log(nums1);
+}
