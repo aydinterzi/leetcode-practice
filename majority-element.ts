84423d9651835ad6968c498ed7a182dec2d413b3
Aydin Terzi <aydinterzi7@gmail.com>
2024-03-20 00:05:18 +0300

    fix: majority element

--- majority-element.ts
+++ majority-element.ts
@@ -1,14 +1,20 @@
 function majorityElement(nums: number[]): number {
   let obj = {};
   let max = 0;
+  let maxIndex;
+  if (nums.length < 2) {
+    return nums[0];
+  }
   for (let i = 0; i < nums.length; i++) {
     if (!Object.keys(obj).includes(nums[i].toString())) {
       obj[nums[i]] = 1;
     } else {
       obj[nums[i]]++;
-      if (obj[nums[i]] > max) {
+      if (max < obj[nums[i]]) {
         max = obj[nums[i]];
+        maxIndex = nums[i];
       }
     }
   }
+  return maxIndex;
 }
